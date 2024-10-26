664838044e16accaa74055a221df19e30236771c
silvante <khamidov.ko@gmail.com>
2024-10-27 02:49:12 +0300

    end for today

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @title = "All Posts"
     @posts = Post.all
   end 
 
@@ -24,13 +25,22 @@ class PostsController < ApplicationController
   def update
     @post = Post.find_by id: params[:id]
     if @post.update get_post_data
-      redirect_to post_path
+      redirect_to post_path(@post)
     else
-      render :new
+      render :edit
     end
   end
+
+  def destroy
+    @post = Post.find(params[:id])  # Find the post by ID
+    @post.destroy                     # Delete the post
+  
+    redirect_to posts_path, notice: "Post was successfully deleted."
+  end
+
+  private
   
   def get_post_data
-    params.require(:post).permit(:tite, :description)
+    params.require(:post).permit(:title, :description)
   end
 end
