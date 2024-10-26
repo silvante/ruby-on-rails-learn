ed9b4d5f1a775144f7aeee1930a599dc4c932c66
silvante <khamidov.ko@gmail.com>
2024-10-26 23:15:05 +0300

    learning

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,18 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(title: params[:title], description: params[:description])
+    
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created!'
+    else
+      render :new, status: :unprocessable_entity
+    end
     render plain: params
+  end
+  
+  def edit
+    @post = Post.find_by id: params[:id]
   end
   
 end
