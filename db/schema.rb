c65a4112a0e04da72392cb6d80582f4719c1848b
silvante <khamidov.ko@gmail.com>
2024-10-29 01:11:43 +0300

    creating user's crud in a seconds

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_25_095325) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_28_215753) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
